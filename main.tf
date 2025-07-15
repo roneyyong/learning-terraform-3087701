e6e3a262f43adf42ae61b20d8773828a226f68bc
roneyyong <80190790+roneyyong@users.noreply.github.com>
2025-07-15 21:26:09 +0800

    auto sfcaling

--- main.tf
+++ main.tf
@@ -37,20 +37,37 @@ module "blog_vpc" {
   }
 }
 
-resource "aws_instance" "blog" {
-  ami                   = data.aws_ami.app_ami.id
-  instance_type         = var.instance_type
+#resource "aws_instance" "blog" {
+#  ami                    = data.aws_ami.app_ami.id
+#  instance_type          = var.instance_type
+#
+#  vpc_security_group_ids = [module.blog_sg.security_group_id]
+#
+#  subnet_id              = module.blog_vpc.public_subnets[0]
+#
+#  tags = {
+#    Name = "HelloWorld"
+#  }
+#}
 
-  vpc_security_group_ids = [module.blog_sg.security_group_id]
+module "autoscaling" {
+  source  = "terraform-aws-modules/autoscaling/aws"
+  version = "9.0.1"
+  # insert the 1 required variable here
 
-  subnet_id = module.blog_vpc.public_subnets[0]
+  name     = "blog"
+  min_size = 1
+  max_size = 2
 
-  tags = {
-    Name = "HelloWorld"
-  }
+  vpc_zone_identifier = module.blog_vpc.public_subnets
+  target_group_arns   = module.blog_alb.target_group_arns
+  security_groups     = [module.blog_sg.security_group_id]
+
+  image_id            = data.aws_ami.app_ami.id
+  instance_type       = var.instance_type
 }
 
-module "alb" {
+module "blog_alb" {
   source = "terraform-aws-modules/alb/aws"
 
   name    = "blog-alb"
